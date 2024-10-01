1639848820df0b18b2fd73a2e51d5218c83a4479
bdalkhalid5s <bdalkhalid5@gmail.com>
2024-10-01 23:59:57 +0600

    nav service added

---
+++ app/components/nav-service/navservice.tsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+const NavService = ({isHover}:any) => {
+
+
+  return (
+    <div>
+        <ul className={`${isHover?"flex":"hidden"} dropdown-menu`} >
+              <li className="text_box">
+                  <h3>All Services</h3>
+                  <p>Delivering Expert, reliable maintenance services</p>
+                  <div className="button_div">
+                      <a className="button" href="#">See all services</a>
+                      <a className="button butto_arrow" href="#"><FontAwesomeIcon icon={faArrowRight} />
+                        
+                        <i className="fa-solid fa-arrow-right"></i></a>
+                  </div>
+              </li>
+            <li className='service-group'>
+              <ul className="service">
+                  <li>
+                    <a href="#" className="dropdown-item">
+                      <Image src="/images/navbar/AC Maintenance.png" alt="" width={48} height={48}/>
+                      <div>
+                      <h5 className='m-0'>AC Maintenance</h5>
+                      <span className='opacit-[.6]'>04 categories</span>
+                      </div>
+                  </a>
+                  </li>
+                  <ul>
+                    <li>
+                      <a href="#" className="dropdown-item">
+                        <Image src="/images/navbar/Fridge Maintenance.png" alt="Fridge Maintenance" width={48} height={48}/>
+                        <div>
+                          <h5 className="m-0">Fridge Maintenance</h5>
+                          <span className="opacity-60">02 categories</span>
+                        </div>
+                      </a>
+                    </li>
+
+                    <li>
+                      <a href="#" className="dropdown-item">
+                        <Image src="/images/navbar/Water leaking.png" alt="Water Leaking" width={48} height={48}/>
+                        <div>
+                          <h5 className="m-0">Water Leaking</h5>
+                          <span className="opacity-60">03 categories</span>
+                        </div>
+                      </a>
+                    </li>
+
+                    <li>
+                      <a href="#" className="dropdown-item">
+                        <Image src="/images/navbar/AC Installation.png" alt="AC Installation" width={48} height={48}/>
+                        <div>
+                          <h5 className="m-0">AC Installation</h5>
+                          <span className="opacity-60">02 categories</span>
+                        </div>
+                      </a>
+                    </li>
+
+                    <li>
+                      <a href="#" className="dropdown-item">
+                        <Image src="/images/navbar/Gas filling service.png" alt="Gas Filling Service" width={48} height={48}/>
+                        <div>
+                          <h5 className="m-0">Gas Filling Service</h5>
+                          <span className="opacity-60">02 categories</span>
+                        </div>
+                      </a>
+                    </li>
+
+                   
+                  </ul>
+              </ul>
+            </li>
+            <li>
+              <ul>
+              <li>
+                  <a href="#" className="dropdown-item">
+                    <Image src="/images/navbar/Custom service .png" alt="Custom Service" width={48} height={48}/>
+                    <div>
+                      <h5 className="m-0">Custom Service</h5>
+                      <span className="opacity-60">00 categories</span>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+    </div>
+  )
+}
+
+export default NavService;
--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,16 +1,26 @@
 'use client'
 import React, { useState } from 'react';
-import './navbar.css';
 import Link from 'next/link';
 import Image from 'next/image';
+import "./navbar.css"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 import logoBlack from '@/public/images/logo/Logo-black.png';
+import NavService from '../nav-service/navservice';
 
 export const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isHover, setIsHover] = useState(false);
+
+    const handleMouseOver = ()=>setIsHover(true)
+    const handleMouseLeave = ()=>setTimeout(()=>setIsHover(false),500)
+
+
   return (
-    <nav className="m-0 h-[95px] w-full z-[99999999999] bg-white navbar-expand-lg">
-      <div className="w-full max-w-[1320px] mx-auto px-4">
+    <nav className="m-0 h-[95px] w-full z-[99999999999] bg-white navbar-expand-lg border-b-2 border-[#F6F6F6]">
+      <div className="w-full max-w-[1320px] mx-auto px-4 flex gap-3 items-center justify-between relative">
 
         {/* <!-- Logo --> */}
         <div className="logo max-w-40 m-5">
@@ -19,387 +29,52 @@ export const Navbar = () => {
             <Image src={logoBlack} alt='logo' className='w-full h-auto' quality={100}/>
           </Link>
         </div>
-        
-        {/* Toggle Button */}
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      
 
-         {/* The collapsible content */}
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-            <li className="nav-item">
-              <a className="nav-link active" href="#" role="button">
-                Home
-              </a>
-            </li>
-
-            <li className="nav-item">
-              <a className="nav-link" href="/about" role="button">
-                About us
-              </a>
-            </li>
-
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="/services" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Services
-                <i className="fa-solid fa-chevron-down" id="btn"></i>
-              </a>
-              <ul className="dropdown-menu">
-                <li className="text_box">
-                  <h3>All Services</h3>
-                  <p>Delivering Expert, reliable maintenance services</p>
-                  <div className="button_div">
-                    <a className="button" href="/services">See all services</a>
-                    <a className="button butto_arrow" href="/services"><i className="fa-solid fa-arrow-right"></i></a>
-                  </div>
-                </li>
-
+        <div>
+            <ul className='flex gap-5'>
                 <li>
-                  <ul className="service">
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/acmaintenanceservice.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <div 
-                          style={{
-                            width: '32px',
-                            height: '32px',
-                            backgroundImage: 'url(./images/navbar/AC Maintenance.png)',
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center'
-                          }} 
-                          aria-label="AC Maintenance"
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>AC Maintenance</h5>
-                          <span style={{ opacity: '.6' }}>04 categories</span>
-                        </div>
-                      </a>
-                    </li>
-
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/fridgemaintenanceservice.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <div 
-                          style={{
-                            width: '32px',
-                            height: '32px',
-                            backgroundImage: 'url(./images/navbar/Fridge Maintenance.png)',
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center'
-                          }} 
-                          aria-label="Fridge Maintenance"
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>Fridge Maintenance</h5>
-                          <span style={{ opacity: '.6' }}>02 categories</span>
-                        </div>
-                      </a>
-                    </li>
-
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/waterleakingservice.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <img 
-                          src="./images/navbar/Water leaking.png" 
-                          alt="" 
-                          style={{ width: '32px', height: '32px' }} 
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>Water leaking</h5>
-                          <span style={{ opacity: '.6' }}>03 categories</span>
-                        </div>
-                      </a>
-                    </li>
-
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/acinstallationservice.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <img 
-                          src="./images/navbar/AC Installation.png" 
-                          alt="" 
-                          style={{ width: '32px', height: '32px' }} 
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>AC Installation</h5>
-                          <span style={{ opacity: '.6' }}>02 categories</span>
-                        </div>
-                      </a>
-                    </li>
-
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/gasfillingservice.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <img 
-                          src="./images/navbar/Gas filling service.png" 
-                          alt="" 
-                          style={{ width: '32px', height: '32px' }} 
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>Gas filling service</h5>
-                          <span style={{ opacity: '.6' }}>02 categories</span>
-                        </div>
-                      </a>
-                    </li>
-                  </ul>
+                    <a href="#">
+                        Home
+                    </a> 
                 </li>
-
-                <li className="etra_service">
-                  <ul>
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/customservice.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <img 
-                          src="./images/navbar/Custom service .png" 
-                          alt="" 
-                          style={{ width: '32px', height: '32px' }} 
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>Custom service</h5>
-                          <span style={{ opacity: '.6' }}>00 categories</span>
-                        </div>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="./pages/allprojects.html" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Projects
-                <i className="fa-solid fa-chevron-down" id="btn"></i>
-              </a>
-              <ul className="dropdown-menu second">
-                <li className="text_box">
-                  <h3>All projects</h3>
-                  <p>Showcasing our key projects that highlight our expertise</p>
-                  <div className="button_div">
-                    <a className="button" href="./pages/allprojects.html">See all projects</a>
-                    <a className="button butto_arrow" href="#"><i className="fa-solid fa-arrow-right"></i></a>
-                  </div>
-                </li>
-
                 <li>
-                  <ul className="service">
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/acinstallationptoject.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <img 
-                          src="./images/navbar/AC Maintenance.png" 
-                          alt="" 
-                          style={{ width: '32px', height: '32px' }} 
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>AC Maintenance</h5>
-                          <span style={{ opacity: '.6' }}>LG company, Doha, Qatar</span>
-                        </div>
-                      </a>
-                    </li>
-
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/waterleakingrepairproject.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <img 
-                          src="./images/navbar/Water leaking.png" 
-                          alt="" 
-                          style={{ width: '32px', height: '32px' }} 
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>Water leaking</h5>
-                          <span style={{ opacity: '.6' }}>Carrier, Al Wakrah, Qatar</span>
-                        </div>
-                      </a>
-                    </li>
-
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/fridgemaintenancerepairproject.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <img 
-                          src="./images/navbar/Fridge Maintenance.png" 
-                          alt="" 
-                          style={{ width: '32px', height: '32px' }} 
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>Fridge repair</h5>
-                          <span style={{ opacity: '.6' }}>Daikin, Al Rayyan, Qatar</span>
-                        </div>
-                      </a>
-                    </li>
-
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/gasfillingmaintenanceproject.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <img 
-                          src="./images/navbar/Gas filling service.png" 
-                          alt="" 
-                          style={{ width: '32px', height: '32px' }} 
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>Gas filling service</h5>
-                          <span style={{ opacity: '.6' }}>Mitsubishi, Madinat ash Shamal, Qatar</span>
-                        </div>
-                      </a>
-                    </li>
-
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/acinstallationptoject.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <img 
-                          src="./images/navbar/AC Installation.png" 
-                          alt="" 
-                          style={{ width: '32px', height: '32px' }} 
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>AC Installation</h5>
-                          <span style={{ opacity: '.6' }}>Sony, Lusail, Qatar</span>
-                        </div>
-                      </a>
-                    </li>
-                  </ul>
+                    <a href="#">
+                        About us
+                    </a> 
                 </li>
-                
-                <li className="etra_service">
-                  <ul>
-                    <li>
-                      <a 
-                        className="dropdown-item" 
-                        href="./pages/gasfillingservice.html" 
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          columnGap: '10px'
-                        }}
-                      >
-                        <img 
-                          src="./images/navbar/Gas filling service.png" 
-                          alt="" 
-                          style={{ width: '32px', height: '32px' }} 
-                        />
-                        <div>
-                          <h5 style={{ margin: '0' }}>Gas filling service</h5>
-                          <span style={{ opacity: '.6' }}>Mitsubishi, Madinat</span>
-                          <br />
-                          <span style={{ opacity: '.6' }}>ash Shamal, Qatar</span>
-                        </div>
-                      </a>
-                    </li>
-                  </ul>
+                <li>
+                    <Link href="#" onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+                        Services
+                    </Link> 
                 </li>
-              </ul>
-            </li>
-
-            <li className="nav-item">
-              <a className="nav-link" href="#" role="button">
-                Career
-              </a>
-            </li>
-
-            <li className="nav-item">
-              <a className="nav-link" href="./pages/contactus.html" role="button">
-                Contact us
-              </a>
-            </li>
-          </ul>
-
-          {/* <!-- number --> */}
-          <div className="navbar_button">
-            <a className="button call-button" href="tel:+97466325019">
-                <i style={{ marginRight: '6px' }} className="fa-solid fa-phone"></i>+974 66325019
-            </a>
-          </div>
-          {/* <!-- number --> */}
+                <li>
+                    <a href="#">
+                        Projects
+                    </a> 
+                </li>
+                <li>
+                    <a href="#">
+                        Career
+                    </a> 
+                </li>
+                <li>
+                    <a href="#">
+                        Contact us
+                    </a>
+                </li>
+            </ul>
         </div>
+
+         <div>
+          <a href="tel:+97466325019" className="bg-[#1c7d9b] p-0 py-[10px] px-[12px] rounded-3xl text-white text-sm leading-normal not-italic tracking-[0.34px] transition duration-400 hover:bg-black hover:text-white flex items-center">
+            <i className="fa-solid fa-phone mr-[6px]"></i> +974 66325019
+          </a>
+        </div>
+
+      <NavService isHover={isHover}/>
+
+
       </div>
     </nav>
   );
